1cbe11208f2e2abfa2e8efdb3462677a3b36a7a3
Jono Hayward <gabs-crests0s@icloud.com>
2025-02-19 12:10:28 +1100

    Update artist.add.js

--- app/artist.add.js
+++ app/artist.add.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 
 import { createClient } from 'redis';
 import { input, select, confirm } from '@inquirer/prompts';
-import { resolve } from './bsky.resolve.js';
+import { resolve, follow } from './bluesky.js';
 import { parse } from './helpers.js';
 
 const main = async () => {

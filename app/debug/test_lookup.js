18c53ff6158894c5375f4693576dbe34ac1a5ce2
Jono Hayward <jono.hayward@icloud.com>
2025-03-12 09:25:41 +1100

    Removed a bunch of old test files

--- app/debug/test_lookup.js
+++
@@ -1,5 +0,0 @@
-import { lookup } from "./lookup.js";
-
-const data = await lookup('mamQeORVY1');
-
-console.log(data);
--- app/debug/test_resolve.js
+++
@@ -1,5 +0,0 @@
-import { resolve } from '../resolve.js';
-
-console.log(await resolve('jono.gay'));
-
-process.exit(0);
